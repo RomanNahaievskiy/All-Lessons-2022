0565aef4cee51db264b2d0751d0d0abbff668178
RomanNahaievskiy <nagaewsky.roma.s@gmail.com>
2022-12-07 22:51:39 +0200

    ++

--- lesson_24/js/script.js
+++ lesson_24/js/script.js
@@ -40,14 +40,99 @@ console.log('====  масив  чи це масив перевірка  isArray 
 console.log(Array.isArray(usersNames));
 console.log('====  масив сортування чисел sort ====');
 let arrTwo = [8, 22, 1];
-console.log(arrTwo.sort());
+console.log(arrTwo.sort()); // Порядок сортування за замовчуванням є зростаючим, побудованим на основі перетворення елементів у рядки, а потім порівняння їхніх послідовностей значень одиниць коду UTF-16.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function compareNumer(a, b) {
+    // console.log(`Порівнюємо ${a} і ${b}`);
+    //     if (a > b) return 1;
+    //     if (a == b) return 0;
+    //     if (a < b) return -1;
 
-function compareNumeric(a, b) {
-    console.log(`Порівнюємо ${a} і ${b}`);
-    if (a > b) return 1;
-    if (a == b) return 0;
-    if (a < b) return -1;
-
-    //return a - b;
+    return a - b;
 }
-console.log(arrTwo.sort(compareNumeric));
+console.log(arrTwo.sort(compareNumer));
+//? compareNumeric - це якась стандартна функція ? - ці назву ми змінюємо - все працює. ! compareFn(a,b)
+//? Яким чином функція отримує аргументи із масиву? - !a і b є аргументами порівняння параметру
+//? Яким чином .sort виконує сортування .sort(тут записується умова ?)!- тут записується виклик або  сама рядкова функція порівняння чисел
+// console.log(arrTwo.sort());
+//? що тоді буде якщо в умові вказати зворотній порядок?
+// console.log(arrTwo.sort());
+console.log("====  масив елементи об`єднуютья в рядок .join ====");
+let usersNamesStr = usersNames.join(';'); // в дужках ми обираємо розділювач
+console.log(usersNamesStr);
+
+// Функції https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions
+console.log('Functions')
+// оголоошення функції
+/* function ім'я(парметри) {
+    !тіло функції
+}
+ім'я повинно бути зрозумілим  що робить функція
+*/
+function showMessage() {
+    console.log('Повідомлення');
+}
+
+// виклик функції
+showMessage();
+
+const numOne = 10;
+const numTwo = 20;
+
+function sumNum(a, b) {
+    return a + b;// return повертає результат
+}
+console.log(sumNum(numOne, numTwo));
+//  Область видимості працює і для функцій. Ящо змінна створена в тілі функцї, то її буде невидно за її межами.
+// змінна може містити функцію. таке присвоєння буде мати назву вункціональний вираз.
+// стрілочні функції мають інший синтаксис let somArrowFunc=(text)=>console.log(text); і її неможливо записати її в кідлька рядок
+// Функція може мати параметр із значенням  за замовчуванням
+
+let somArrowFunc = (text = "Здрастье!") => console.log(text);
+somArrowFunc('Привіт');
+somArrowFunc();
+
+
+// ==================== DOM =========================== )DOOM  або Dominic Toretto
+
+
+// WINDOW
+let wWidht = window.innerWidth;  // ширина вікна в'юпорта
+let wHeight = window.innerHeight;  // висота  вікна в'юпорта
+
+// Navigator 
+console.log(navigator.userAgent);//інформація про машину +браузер клієнта
+if (navigator.userAgent.includes('Chrome')) {
+    console.log('Chrome Browser');
+} else if (navigator.userAgent.includes('Firefox')) {
+    console.log('Firefox Browser');
+}
+// Location - доступ до адресного рядка
+
+console.log(location.href);
+// location.href = 'https://google.com'--- перенаправлення користувача
+
+// Alert
+// alert('Увага повідомлення');
+// Confirm
+// let confirmAnswer = confirm("Очікування підтвердження") Поверне TRUE або FALSE залежно від вибору клієнта
+// Prompt
+// let promptAnswer=prompt("Запитання"); - поверне рядок, якщо так  або null, якщо ні
+
+// Documetnm Object Model
+// Об'єктна модель документа (cheat codes) - чіти )) 
+// !важливо не змінювати назви змінних
+const htmlElement = document.documentElement;// отримати увесь html код 
+const headElement = document.head; // отримати тег head
+const bodyElement = document.body; // отримати тег body
+const firstfChildNode = bodyElement.firstfChildNode; // отримати перший дочірній елемент body який не очевидний якийсь відступ табуляція
+const lastChildNode = bodyElement.lastfChildNode; // отримати останній дочірній елемент body який не очевидний якийсь відступ табуляція
+
+const firstChild = bodyElement.firstElementChild; //отримати перший дочірній елемент
+const lastfChild = bodyElement.lastElementChild; //отримати останній дочірній елемент важливо не змінювати назви змінних
+
+const childNodes = bodyElement.children; //усі дочірні елементи (колекція) схоже на масив, але не всі методи масивів працюють з колекціями  
+console.log(childNodes);
+for (let childNode of childNodes) {
+    console.log(childNode);
+}
