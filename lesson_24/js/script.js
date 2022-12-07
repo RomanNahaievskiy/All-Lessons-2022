8081c34d94ca6ed99c9f1482ce6e9618a4ca3eb0
RomanNahaevskiy <nagaewsky.roma.s@gmail.com>
2022-12-07 18:42:48 +0200

    ++

--- lesson_24/js/script.js
+++ lesson_24/js/script.js
@@ -1,2 +1,10 @@
 "use strict" // Врубаємо Строгий режим.
-console.log('====   Вчимо JS    ====\n====    Урок 24    ====')
+console.log('====   Вчимо JS    ====\n====    Урок 24    ====');
+
+// масиви
+let someArray = [10, 20, 5];
+console.log(typeof someArray);
+console.log(someArray);
+console.log(someArray.length);
+
+let sdersName = ["Рома", "Вадим", "Аліна"];
