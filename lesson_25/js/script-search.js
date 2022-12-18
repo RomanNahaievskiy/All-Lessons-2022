572d663f9b8ee2de1f8ad979e6d052ec32cae24e
RomanNahaievskiy <nagaewsky.roma.s@gmail.com>
2022-12-18 23:49:51 +0200

    ))

---
+++ lesson_25/js/script-search.js
@@ -0,0 +1,40 @@
+"use strict"; // вмикаю строгий режим
+// створюю лічильник символів поля вводу :
+const searchText = document.querySelector('.search__input');// отримую елемент input
+const symbolLimit = searchText.getAttribute('maxlength');// отримую щначення атрибута maxlength
+const symCounter = document.querySelector('.search__symb-counter span');// звертаюся до  span 
+// в елемнеті з класом search-symb-counter
+symCounter.innerHTML = symbolLimit; // призначаю span'у значення, отримане із атрибута maxlength
+// елемента input
+
+searchText.addEventListener("keyup", setSymbCounter);// призначаю input`у обробник подій типу відпускання клавіші
+searchText.addEventListener("keydown", function (event) { //призначаю input`у обробник подій типу натискання клавіші
+    if (event.repeat) {// перевіряю чи є автоповтор (затиснення клавіші)
+        setSymbCounter();// якщо так, викликаю функцію лічильника
+    }
+});
+// Опис функції роботи лічильника
+function setSymbCounter() {
+    const symbLeftLimit = symbolLimit - searchText.value.length;
+    symCounter.innerHTML = symbLeftLimit;
+}
+
+const serchIcon = document.querySelector('.search__icon');
+const searchForm = document.querySelector('.search__form')
+document.addEventListener("click", searchActive);
+
+document.addEventListener("keyup", function (event) {
+    if (event.code === 'Escape');
+    serchIcon.classList.remove('_active');
+    searchForm.classList.remove('_active');
+})
+function searchActive(event) {
+    if (event.target.closest('.search__icon')) {
+        serchIcon.classList.toggle('_active');
+        searchForm.classList.toggle('_active');
+    }
+    if (!event.target.closest('.search__icon') && (!event.target.closest('.search__form'))) {
+        serchIcon.classList.remove('_active');
+        searchForm.classList.remove('_active');
+    }
+}
